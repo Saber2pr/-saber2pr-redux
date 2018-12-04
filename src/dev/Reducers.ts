624c74529e7112628a2bb2c44da79bed52d57fe7
saber2pr <saber2pr@gmail.com>
2018-12-04 16:47:57 +0800

    update

--- src/dev/Reducers.ts
+++ src/dev/Reducers.ts
@@ -2,12 +2,13 @@
  * @Author: AK-12
  * @Date: 2018-12-03 23:38:12
  * @Last Modified by: AK-12
- * @Last Modified time: 2018-12-04 16:35:46
+ * @Last Modified time: 2018-12-04 16:46:24
  * @discript: 存放reducers的空间
  */
 import * as Types from '../types/Types'
 import * as Actions from './Actions'
 import { State } from './State'
+import { message } from './reducers/infor/message'
 
 // title reducer
 export let title: Types.IReducer<
@@ -49,21 +50,6 @@ export let infor: Types.IReducer<
       return { name: state.name, message: message(state.message, action) }
     case 'reduce_message_p2':
       return { name: state.name, message: message(state.message, action) }
-    default:
-      return state
-  }
-}
-
-// infor_message reducer
-let message: Types.IReducer<
-  Types.IState['infor']['message'],
-  Types.IAction<Actions.Infor>
-> = (state = State.infor.message, action) => {
-  switch (action.type) {
-    case 'reduce_message_p1':
-      return { p1: state.p1 + action.value, p2: state.p2 }
-    case 'reduce_message_p2':
-      return { p1: state.p1, p2: state.p2 + action.value }
     default:
       return state
   }
