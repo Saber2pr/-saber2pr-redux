f0713ef93b52ea7a3f3d90c3f6c165a5f11bdf2f
saber2pr <saber2pr@gmail.com>
2018-12-04 14:49:13 +0800

    update

--- src/types/IReducers.ts
+++ src/types/IReducers.ts
@@ -1,12 +1,16 @@
 /*
  * @Author: AK-12
  * @Date: 2018-12-04 14:46:33
- * @Last Modified by:   AK-12
- * @Last Modified time: 2018-12-04 14:46:33
+ * @Last Modified by: AK-12
+ * @Last Modified time: 2018-12-04 14:48:53
  */
+import { IState } from './IState'
 import { IAction, IAction_Type_Default } from './IAction'
 
 // reducer
-export interface IReducer<S = S, A = IAction<IAction_Type_Default, string>> {
+export interface IReducer<
+  S = IState,
+  A = IAction<IAction_Type_Default, string>
+> {
   (state: S, action: A): S
 }
