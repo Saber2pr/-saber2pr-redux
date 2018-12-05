12e93fd35ca2d3903c719ee0f7ba620bad0a8e91
saber2pr <saber2pr@gmail.com>
2018-12-05 13:42:20 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@
  * @Author: AK-12
  * @Date: 2018-12-03 23:25:19
  * @Last Modified by: AK-12
- * @Last Modified time: 2018-12-05 13:17:29
+ * @Last Modified time: 2018-12-05 13:42:07
  */
-import { $Store } from './dev/Build'
-import { IAction_default } from './types/Types'
+import { $Store } from './redux/dev/Build'
+import { IAction_default } from './redux/types/Types'
 
 // 监听回调
 let listener = () => console.log($Store.getState())
