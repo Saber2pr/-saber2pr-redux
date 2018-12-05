ce416ab6efabb089c7c557a1ee2b1fda451a15f2
saber2pr <saber2pr@gmail.com>
2018-12-05 13:37:51 +0800

    update doc

--- src/types/IAction.ts
+++ src/types/IAction.ts
@@ -2,7 +2,7 @@
  * @Author: AK-12
  * @Date: 2018-12-05 12:46:04
  * @Last Modified by: AK-12
- * @Last Modified time: 2018-12-05 13:15:26
+ * @Last Modified time: 2018-12-05 13:28:27
  */
 /**
  * IAction接口
@@ -11,13 +11,20 @@
 import { Action } from 'redux'
 import { Type_default } from './IAction_type'
 /**
- *IAction_default
+ *IAction
  *
  * @export
  * @interface IAction
  * @extends {Action<keyof Default>}
  */
-export interface IAction_default<T = string>
-  extends Action<keyof Type_default> {
-  value: T
+export interface IAction<T, V = string> extends Action<keyof T> {
+  value: V
 }
+/**
+ *IAction_default
+ *
+ * @export
+ * @interface IAction_default
+ * @extends {IAction<Type_default>}
+ */
+export interface IAction_default extends IAction<Type_default> {}
